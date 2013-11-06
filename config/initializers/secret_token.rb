03d4c7d80562d3353bd78c7509b6c3f261e52cd3
joyicecloud <joyicecloud@gmail.com>
2013-11-05 21:22:37 -0800

    Clean slate with just rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bettertogether::Application.config.secret_key_base = '2db1e0b88fe1f457af9122246520daa4360149755fc77728d2bcffc9b2496c508108d89dd33907d2576d59425f90a3ed13a8123b32c7e3fed317f252c0a31f4f'
