35d477898d7a1df272e9c49ec2d713fc8635de6d
joyicecloud <joyicecloud@gmail.com>
2013-11-15 17:26:48 -0800

    Tests to make sure the creation of a goal works

---
+++ spec/requests/goal_pages_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe "Goal Pages" do
+
+  subject { page }
+
+  describe "homepage to create goal" do
+    before { visit root_path }
+
+    it { should have_title('Better Together') }
+    it { should have_link('Save Goal') }
+    it { should have_button('Sign in') }
+    it { should have_button('Set Goal') }
+
+    let(:submit) { "Set Goal" }
+
+    describe "with invalid information for setting goal" do
+
+      it "should not create a goal" do
+        expect { click_button submit }.not_to change(Goal, :count)
+      end
+
+      before { click_button "Set Goal" }
+
+      it { should have_title('Better Together') }
+      it { should have_selector('div.alert.alert-error', text: 'friggin') }
+
+      describe "after visiting another page should not display flash" do
+        before { click_link "Better Together" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+    end
+
+    describe "with valid information for setting goal" do
+      let(:goal) { FactoryGirl.create(:goal) }
+      before do
+        fill_in "goal_goal", with: "I want to make 300 recipes in 365 days"
+      end
+
+      it "should create a goal" do
+        expect { click_button submit }.to change(Goal, :count).by(1)
+      end
+    end
+  end
+
+  describe "goal list page" do
+
+    let(:goal) { FactoryGirl.create(:goal) }
+
+    before { visit goal_path(goal) }
+
+    it { should have_selector('h1', 'Your Goals') }
+    it { should have_title('Goals') }
+  end
+end
