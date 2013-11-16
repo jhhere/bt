6101f9192b31159d9e378ec3b39bc315302faee5
joyicecloud <joyicecloud@gmail.com>
2013-11-15 22:12:53 -0800

    Add User model, bcrypt gem, validations and tests for email and password attributes

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+
+  validates_presence_of :email
+  validates_uniqueness_of :email, case_sensitive: false
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates_format_of :email, with: VALID_EMAIL_REGEX
+
+  has_secure_password
+  validates_presence_of :password
+  validates_length_of :password, minimum: 6
+end
