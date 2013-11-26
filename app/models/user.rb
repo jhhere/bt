4fc73751c161d30e53f4a14cb99e6ad3e12822f0
joyicecloud <joyicecloud@gmail.com>
2013-11-25 19:52:18 -0800

    Add Twitter sign-in

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2, :facebook]
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:google_oauth2, :facebook, :twitter]
 
   has_many :goals, dependent: :destroy
 
@@ -13,7 +13,7 @@ class User < ActiveRecord::Base
   end
 
   def email_required?
-    guest ? false : super
+    guest || (uid && provider) ? false : super
   end
 
   def self.find_for_google_oauth2(access_token, signed_in_resource=nil)
@@ -32,6 +32,19 @@ class User < ActiveRecord::Base
     user = User.where(:provider => auth.provider, :uid => auth.id).first
     unless user
       user = User.create(name:auth.extra.raw_info.name,
+                         provider:auth.provider,
+                         uid:auth.uid,
+                         email:auth.info.email,
+                         password:Devise.friendly_token[0,20]
+                         )
+    end
+    user
+  end
+
+  def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
+    user = User.where(:provider => auth.provider, :uid => auth.id).first
+    unless user
+      user = User.create!(name:auth.info.name,
                          provider:auth.provider,
                          uid:auth.uid,
                          email:auth.info.email,
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131125060951) do
+
+ActiveRecord::Schema.define(version: 20131126202206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
