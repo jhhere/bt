9e0afaac40bdebe1e0ac34d04dc581b5ec8f3c87
joyicecloud <joyicecloud@gmail.com>
2013-12-12 00:29:22 -0800

    Add js form to controller and highlighting for goal item

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -7,12 +7,15 @@ class GoalsController < ApplicationController
   def create
     sign_in(User.create(guest: true)) unless user_signed_in?
     @goal = current_user.goals.new(goal_params)
-    if @goal.save
-      flash[:success] = "Goal created!"
-      redirect_to current_user
-    else
-      flash.now[:error] = "That's not a friggin' goal!"
-      render 'new'
+
+    respond_to do |format|
+      if @goal.save
+        format.html { redirect_to current_user }
+        format.js { @current_goal = @goal }
+      else
+        flash.now[:error] = "That's not a friggin' goal!"
+        format.html { render action: 'new' }
+      end
     end
   end
 
