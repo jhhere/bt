6505b822bfbecc27a7e43a6cf294051a28c4f7e0
joyicecloud <joyicecloud@gmail.com>
2013-12-11 18:01:46 -0800

    Fix typo in fb method allow ability to sign in and out

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,7 +29,7 @@ class User < ActiveRecord::Base
   end
 
   def self.find_for_facebook_oauth(auth, signed_in_resource=nil)
-    user = User.where(:provider => auth.provider, :uid => auth.id).first
+    user = User.where(:provider => auth.provider, :uid => auth.uid).first
     unless user
       user = User.create(name:auth.extra.raw_info.name,
                          provider:auth.provider,
@@ -44,7 +44,7 @@ class User < ActiveRecord::Base
   def self.find_for_twitter_oauth(auth, signed_in_resource=nil)
     user = User.where(:provider => auth.provider, :uid => auth.id).first
     unless user
-      user = User.create!(name:auth.info.name,
+      user = User.create(name:auth.info.name,
                          provider:auth.provider,
                          uid:auth.uid,
                          email:auth.info.email,
