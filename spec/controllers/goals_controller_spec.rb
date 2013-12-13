8e3f7fcf4dda685f84c04ace4a394bf161978114
joyicecloud <joyicecloud@gmail.com>
2013-12-12 18:36:21 -0800

    Controller test for ajax

--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -1,1 +1,19 @@
 require 'spec_helper'
+
+describe GoalsController do
+
+  let(:user) { FactoryGirl.create(:user) }
+  let(:goal) { FactoryGirl.create(:goal) }
+
+  it "create a goal on the user profile page via ajax" do
+    xhr :post, :create, goal: { goal: goal }
+    expect(response).to be_success
+  end
+
+  it "create a goal increments the count" do
+    expect do
+      xhr :post, :create, goal: { goal: goal }
+    end.to change(Goal, :count).by(2)
+  end
+
+end
