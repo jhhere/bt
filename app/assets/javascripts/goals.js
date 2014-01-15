91d86447deae3850745b99477d5c5b0869565baf
joyicecloud <joyicecloud@gmail.com>
2014-01-14 23:21:40 -0800

    Goes up and down

--- app/assets/javascripts/goals.js
+++ app/assets/javascripts/goals.js
@@ -10,18 +10,25 @@ jQuery(function() {
   });
 
   $('#user_goals').on('click', 'li.goal button', function(e) {
+
     button = $(e.target);
+
+    if (button.parents('button').length ) {
+      button = button.parents('button');
+    }
+
     goal = button.parents('li.goal')
 
     if (button.hasClass("up")) {
       prevGoal = goal.prev('li.goal');
       console.log(prevGoal);
       prevGoal.insertAfter(goal);
-     } else {
+    } else {
       nextGoal = goal.next('li.goal');
       console.log(nextGoal);
       nextGoal.insertBefore(goal);
-     }
+    }
+    $.post($('#user_goals').data('update-url'), $('#user_goals').sortable('serialize'));
   });
 
 });
